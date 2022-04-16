e05e77e9c8d13fd13eb00f5b50003168bccd6a08
YounKD <88384827+YounKD@users.noreply.github.com>
2022-04-17 01:24:51 +0900

    20220417 test

--- miniProject-team-burgerhi/src/team/burgerhi/run/Application.java
+++ miniProject-team-burgerhi/src/team/burgerhi/run/Application.java
@@ -5,7 +5,7 @@ import team.burgerhi.kiosk.views.OrderMenu;  // PUSH-TEST Ver2
 
 public class Application {
 	public static void main(String[] args) {
-		
+		/* 담담 */
 		/* View 화면이 보여지도록 인스턴스 생성 및 메소드 호출 */
 		OrderMenu orderMenu = new OrderMenu();
 		orderMenu.displayMainMenu();
