6d6382d81bbe83b9c1518b4a1cb6ae4b99ea6137
DESKTOP-Q22BSFF\bj062 <bjkang2181@gmail.com>
2022-04-20 15:57:23 +0900

    no message

--- miniProject-team-burgerhi/src/team/burgerhi/run/Application.java
+++ miniProject-team-burgerhi/src/team/burgerhi/run/Application.java
@@ -7,6 +7,6 @@ public class Application {
 		
 		/* view 화면이 보여지도록 인스턴스 생성 및 메소드 호출 */
 		OrderMenu orderMenu = new OrderMenu();
-		orderMenu.displayMainMenu(); // test
+		orderMenu.displayMainMenu(); // test tt
 	}
 }
